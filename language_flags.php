ddc9c4724053e485eb513cba0d9209ca4954e9bf
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2023-01-07 22:08:57 +0100

    Add language_flags.php

---
+++ language_flags.php
@@ -0,0 +1,242 @@
+<?php
+	// https://reccacon.com/Ukraine
+	$languages_02bd = array(
+		"Ukraine+02bd"                  => ""
+	);
+
+	// http://sog.caliban.org/
+    $languages_118d = array(
+		"android+118d"                  => "🇬🇧",
+		"brasil+118d"                   => "🇵🇹",
+		"deutsch+118d"                  => "🇩🇪",
+		"espanol+118d"                  => "🇪🇸",
+		"francais+118d"                 => "🇫🇷",
+		"gaming+118d"                   => "🇬🇧",
+		"health+118d"                   => "🇬🇧",
+		"im+118d"                       => "🇬🇧",
+		"italiano+118d"                 => "🇮🇹",
+		"linux+118d"                    => "🇬🇧",
+		"modernsurvival+118d"           => "🇬🇧",
+		"music+118d"                    => "🇬🇧",
+		"nl+118d"                       => "🇳🇱",
+		"persian+118d"                  => "🇮🇷",
+		"portugues+118d"                => "🇵🇹",
+		"privacy+118d"                  => "🇬🇧",
+		"russian+118d"                  => "🇷🇺",
+		"security+118d"                 => "🇬🇧",
+		"snops+118d"                    => "🇬🇧",
+		"sogops+118d"                   => "🇬🇧",
+		"travel+118d"                   => "🇬🇧",
+		"uk+118d"                       => "🇬🇧",
+		"unix+118d"                     => "🇬🇧"
+    );
+
+	// http://45.77.102.159/
+	$languages_132d = array(
+		"kat67world+132d"               => "🇬🇧",
+		"origin+132d"                   => "🇨🇳",
+		"scammer+132d"                  => "🇬🇧"
+	);
+
+	// https://3399981.xyz/
+	$languages_13f6 = array(
+		"bangladesh+13f6"               => "🇧🇩",
+		"czsk+13f6"                     => "🇨🇿",
+		"india+13f6"                    => "🇮🇳",
+		"indonesia+13f6"                => "🇮🇩",
+		"korea+13f6"                    => "🇰🇷",
+		"poland+13f6"                   => "🇵🇱",
+		"programming+13f6"              => "🇬🇧",
+		"womanbodybeauty+13f6"          => "🇬🇧"
+	);
+
+	// http://170.64.140.202/
+	$languages_15a0 = array(
+		"ufo+15a0"                      => ""
+	);
+
+	// http://116.203.217.101/
+	$languages_2054 = array(
+		"feels+2054"                    => "🇬🇧",
+		"pol+2054"                      => "🇬🇧",
+		"tech+2054"                     => "🇬🇧",
+		"tools+2054"                    => "🇬🇧"
+	);
+
+	// http://173.249.51.184/
+	$languages_2812 = array(
+		"fi+2812"                       => "🇫🇮",
+		"main+2812"                     => "",
+		"scandinavia+2812"              => ""
+	);
+
+	// http://sogs.horse.xxx/
+	$languages_2b92 = array(
+		"biz+2b92"                      => "🇬🇧",
+		"gee+2b92"                      => "🇬🇧",
+		"guns+2b92"                     => "🇬🇧"
+	);
+
+	// https://2hu-ch.org/
+	$languages_49ac = array(
+		"animu+49ac"                    => "🇬🇧",
+		"cryptography+49ac"             => "🇬🇧"
+	);
+
+	// http://20.168.47.58/
+	$languages_52bc = array(
+		"australia+52bc"                => "🇬🇧",
+		"Bipolar+52bc"                  => "🇬🇧",
+		"General+52bc"                  => "🇬🇧"
+	);
+
+	// https://movistar.ht-rewrite.com/
+	$languages_70d0 = array(
+		"internet+70d0"                 => "🇬🇧"
+	);
+
+	// http://session.mathnodes.com/
+	$languages_8585 = array(
+		"mathnodes-dvpn-oxen-dero+8585" => "🇬🇧"
+	);
+
+	// http://95.217.236.112/
+	$languages_8e2e = array(
+		"CrazyGamers+8e2e"              => "",
+		"fishing+8e2e"                  => "",
+		"ohjelmointisuomi+8e2e"         => "🇫🇮",
+		"pk+8e2e"                       => "🇫🇮",
+		"SuomenTori+8e2e"               => "🇫🇮",
+		"suomentoriv2+8e2e"             => "🇫🇮",
+		"tori+8e2e"                     => ""
+	);
+
+	// https://open.getsession.org/
+	$languages_a03c = array(
+		"crypto+a03c"                   => "🇬🇧",
+		"lokinet+a03c"                  => "🇬🇧",
+		"oxen+a03c"                     => "🇬🇧",
+		"session-farsi+a03c"            => "🇮🇷",
+		"session-updates+a03c"          => "🇬🇧",
+		"session+a03c"                  => "🇬🇧"
+	);
+
+	//
+	$languages_a19a = array(
+		"china+a19a"                    => "🇨🇳"
+	);
+
+	// http://185.130.45.173/
+	$languages_b79f = array(
+		"cryptography+b79f"             => "🇬🇧",
+		"philosophy+b79f"               => "🇬🇧",
+		"trading+b79f"                  => "🇬🇧",
+		"uncensored+b79f"               => "🇬🇧",
+		"webdev+b79f"                   => "🇬🇧"
+	);
+
+	//
+	$languages_c01b = array(
+		"ares-races+c01b"               => "🇬🇧",
+		"cfm+c01b"                      => "🇬🇧",
+		"contesting+c01b"               => "🇬🇧",
+		"cw+c01b"                       => "🇬🇧",
+		"dmr+c01b"                      => "🇬🇧",
+		"dstar+c01b"                    => "🇬🇧",
+		"hamradio+c01b"                 => "🇬🇧",
+		"hf+c01b"                       => "🇬🇧",
+		"ka7vee-w7sj+c01b"              => "🇬🇧",
+		"ralph_june+c01b"               => "🇬🇧",
+		"repeaters+c01b"                => "🇬🇧"
+	);
+
+	// http://open.session.codes/
+	$languages_c7fb = array(
+		"4c2ba1+c7fb"                   => "🇪🇸",
+		"6e7b5f+c7fb"                   => "",
+		"90ca71+c7fb"                   => "🇷🇺",
+		"a80d69+c7fb"                   => "🇬🇧",
+		"d8dc40+c7fb"                   => "🇪🇸",
+		"edb497+c7fb"                   => "🇨🇳",
+		"sc+c7fb"                       => ""
+	);
+
+	// http://95.179.250.131/
+	$languages_e5e0 = array(
+		"generalchat+e5e0"              => "🇬🇧",
+		"nsfw+e5e0"                     => "🇬🇧",
+		"public+e5e0"                   => "🇬🇧"
+	);
+
+	// https://sog.zcyph.cc/
+	$languages_e56f = array(
+		"offtopic+e56f"                 => "🇬🇧"
+	);
+
+	// http://remote.noff.co:8700/
+	$languages_ea8f = array(
+		"Memes+ea8f"                    => "🇬🇧"
+	);
+
+	// http://13.233.251.36:8081/
+	$languages_efca = array(
+		"xyz+efca"                      => ""
+	);
+
+	// http://45.33.57.83/
+	$languages_efe1 = array (
+		"palmos+efe1"                   => "🇬🇧",
+		"radcom+efe1"                   => "🇬🇧",
+		"ticalc+efe1"                   => "🇬🇧",
+		"winmo+efe1"                    => "🇬🇧"
+	);
+
+	// http://117.186.115.186:8888/
+	$languages_fe93 = array(
+		"android+fe93"                  => "🇨🇳",
+		"cantonese+fe93"                => "🇭🇰",
+		"education+fe93"                => "🇨🇳",
+		"foss+fe93"                     => "🇨🇳",
+		"health+fe93"                   => "🇨🇳",
+		"im+fe93"                       => "🇨🇳",
+		"linux+fe93"                    => "🇨🇳",
+		"literature+fe93"               => "🇨🇳",
+		"mandarin+fe93"                 => "🇨🇳",
+		"music+fe93"                    => "🇨🇳",
+		"philosophy+fe93"               => "🇨🇳",
+		"privacy+fe93"                  => "🇨🇳",
+		"sci-tech+fe93"                 => "🇨🇳",
+		"security+fe93"                 => "🇨🇳",
+		"session+fe93"                  => "🇨🇳"
+	);
+
+	$languages = array_merge(
+		$languages_02bd,
+		$languages_118d,
+		$languages_132d,
+		$languages_13f6,
+		$languages_15a0,
+		$languages_2054,
+		$languages_2812,
+		$languages_2b92,
+		$languages_49ac,
+		$languages_52bc,
+		$languages_70d0,
+		$languages_8585,
+		$languages_8e2e,
+		$languages_a03c,
+		$languages_a19a,
+		$languages_b79f,
+		$languages_c01b,
+		$languages_c7fb,
+		$languages_e5e0,
+		$languages_e56f,
+		$languages_ea8f,
+		$languages_efca,
+		$languages_efe1,
+		$languages_fe93
+	);
+
+//	$languages_json = json_encode($languages);
+//	file_put_contents("languages.json", $languages_json);
+?>
